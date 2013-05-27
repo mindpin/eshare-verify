ccc765009a473c42e94d0b07e95a3788651653f5
fushang318 <fushang318@gmail.com>
2013-05-27 13:49:35 +0800

    init rails

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.12' # RAILS #不要更新 3.2.13 有性能问题，等 3.2.14
+gem 'mysql2', '0.3.11' # MYSQL数据库连接
+gem 'json', '1.7.7' # JSON解析，RAILS默认引入的
+
+group :assets do
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'turbo-sprockets-rails3', '0.3.2' # 加速预编译
+end
+
+#gem 'jquery-rails', '2.2.1'
+#gem 'jquery-ui-rails', '4.0.2'
+#gem 'unicorn', '4.6.2'
+
+# 页面渲染
+gem 'haml', '4.0.0' # HAML模板语言
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EshareVerify::Application.config.secret_token = 'af171182244a75ad8d981c3f5e1cd84209eab8693dfa00befe5f7dff21944ecd3618685c277baf3a357fe0aa07c5f09232f7da696658731417394dc9ebcf40fe'
